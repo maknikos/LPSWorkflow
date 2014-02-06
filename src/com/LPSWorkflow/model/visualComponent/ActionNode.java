873149b30b027a7da31fbc77190d76be3441fbc9
elijah6 <elijah.rla@gmail.com>
2014-02-06 15:41:48 +0000

    Macro-actions have a label for the name

--- src/com/LPSWorkflow/model/visualComponent/ActionNode.java
+++ src/com/LPSWorkflow/model/visualComponent/ActionNode.java
@@ -1,31 +1,56 @@
 package com.LPSWorkflow.model.visualComponent;
 
+import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 
 /**
  * Visual component representing an action
  */
 public class ActionNode extends Node {
+    private Group group;
     private Label text;
     private HBox goalDefinition;
 
     public ActionNode(String name, HBox goalDefinition) {
         super(name);
         // TODO remove test
-        this.goalDefinition = goalDefinition;
 
         if(goalDefinition != null){
             goalDefinition.setStyle("-fx-border-color:black; -fx-padding:18px;");
-            getChildren().addAll(goalDefinition);
+            this.goalDefinition = goalDefinition;
         } else {
-            text = new Label(name);
-            text.setStyle("-fx-border-color:black; -fx-padding:18px; -fx-font-size:25px");
-
-            getChildren().addAll(text);
+            this.goalDefinition = new HBox();
         }
 
+        this.group = new Group();
+        this.group.setStyle("-fx-border-color:black; -fx-padding:18px;");
 
+        this.text = new Label(name);
+        this.text.setStyle("-fx-border-color:black; -fx-padding:18px; -fx-font-size:25px");
+
+        group.getChildren().addAll(this.text, this.goalDefinition);
+        getChildren().add(group);
+
+//        this.setOnMouseDragged(new EventHandler<MouseEvent>() {
+//            @Override
+//            public void handle(MouseEvent mouseEvent) {
+//                System.out.println(
+//                        String.format("%s is being dragged to (%f:%f) which is (%f:%f)",
+//                                getName(), mouseEvent.getSceneX(), mouseEvent.getSceneY(),
+//                                mouseEvent.getX(), mouseEvent.getY()
+//                        ));
+//            }
+//        });
+//
+//        this.setOnMouseClicked(new EventHandler<MouseEvent>() {
+//            @Override
+//            public void handle(MouseEvent mouseEvent) {
+//
+//            }
+//        });
     }
 
     //TODO clear up
