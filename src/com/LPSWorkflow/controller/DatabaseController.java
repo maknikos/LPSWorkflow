dc7dc7e969288fbb0ade1eb94e6c3f9a9a3beba1
elijah6 <elijah.rla@gmail.com>
2014-02-26 07:31:30 +0000

    Clearing up small bits

--- src/com/LPSWorkflow/controller/DatabaseController.java
+++ src/com/LPSWorkflow/controller/DatabaseController.java
@@ -4,7 +4,6 @@ import com.LPSWorkflow.model.database.Database;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextArea;
-import javafx.scene.layout.Pane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
