cd574a63ce1adc3b20f16a70830d7a75ac413548
elijah6 <elijah.rla@gmail.com>
2014-03-30 21:24:45 +0100

    messageView introduced on top of canvasView

---
+++ src/com/LPSWorkflow/controller/MessageViewController.java
@@ -0,0 +1,14 @@
+package com.LPSWorkflow.controller;
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Controller for the message view
+ */
+public class MessageViewController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+}
--- src/com/LPSWorkflow/main/Controller.java
+++ src/com/LPSWorkflow/main/Controller.java
@@ -1,4 +1,24 @@
 package com.LPSWorkflow.main;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.Pane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable{
+    @FXML
+    private Pane mainPane;
+
+    @FXML
+    private SplitPane mainSplitPane;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        //manually bind width and height so that it fills the parent pane
+        mainSplitPane.prefWidthProperty().bind(mainPane.widthProperty());
+        mainSplitPane.prefHeightProperty().bind(mainPane.heightProperty());
+    }
 }
