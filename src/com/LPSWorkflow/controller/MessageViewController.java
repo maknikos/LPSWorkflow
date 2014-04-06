003148cfaf2370dc719c37c3159093a3d0b356c0
elijah6 <elijah.rla@gmail.com>
2014-04-06 14:53:32 +0100

    if there are more than 2 messages, they are grouped into a list ('more' button)

--- src/com/LPSWorkflow/controller/MessageViewController.java
+++ src/com/LPSWorkflow/controller/MessageViewController.java
@@ -4,12 +4,15 @@ import com.LPSWorkflow.model.message.Message;
 import com.LPSWorkflow.model.message.MessageData;
 import com.LPSWorkflow.model.message.MessageShape;
 import com.LPSWorkflow.model.message.MessageType;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
 import java.net.URL;
@@ -21,25 +24,56 @@ import java.util.ResourceBundle;
 public class MessageViewController implements Initializable {
     @FXML
     private VBox messageBox;
+
     private MessageData messageData;
+    private Label moreButton;
+    private StringProperty msgCountStr = new SimpleStringProperty("More (0 messages)");
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        moreButton = createMoreButton();
         messageData = MessageData.getInstance();
         messageData.messageListProperty().addListener(new ChangeListener<ObservableList<Message>>() {
             @Override
-            public void changed(ObservableValue<? extends ObservableList<Message>> observableValue, ObservableList<Message> messages, ObservableList<Message> messages2) {
-                //TODO display two messages max. and show an option to display all
-                messageBox.getChildren().add(createMessage("ERROR: the file format '.pl' is not supported."));
+            public void changed(ObservableValue<? extends ObservableList<Message>> observableValue, ObservableList<Message> oldMessages, ObservableList<Message> messages) {
+                // sync with messages stored in MessageData instance
+                messageBox.getChildren().clear();
+
+                // only display 2 messages, and group the rest
+                int count = 0;
+                for(Message m : messages){
+                    count++;
+
+                    if(count > 2){
+                        // TODO make a group/list of messages
+                        messageBox.getChildren().add(moreButton);
+                        msgCountStr.setValue("More (" + messages.size() + " messages)");
+                        break;
+                    }
+                    messageBox.getChildren().add(createMessage(m));
+                }
             }
         });
     }
 
-    private MessageShape createMessage(String message) {
-        return new MessageShape(new Message(message, MessageType.ERROR));
+    private Label createMoreButton() {
+        Label moreLabel = new Label();
+        moreLabel.textProperty().bind(msgCountStr);
+        moreLabel.setStyle("-fx-border-color:#FF9999BB; -fx-border-radius:5px; " +
+                "-fx-background-color:#FF999999; -fx-background-radius:5px;" +
+                "-fx-text-fill:#771111FF; -fx-font-size:11px;" +
+                "-fx-padding:2 10 2 10");
+        return moreLabel;
     }
 
+    private MessageShape createMessage(Message message) {
+        return new MessageShape(message);
+    }
+
+    //***** TEST ****** TODO clear up
+    private int count = 0;
     public void handleTest(ActionEvent actionEvent) {
-        messageData.getMessageList().add(new Message("Message list", MessageType.ERROR));
+        messageData.getMessageList().add(new Message("This is an important message: Message list " + count, MessageType.ERROR));
+        count++;
     }
 }
--- src/com/LPSWorkflow/model/message/MessageShape.java
+++ src/com/LPSWorkflow/model/message/MessageShape.java
@@ -6,7 +6,6 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
 
@@ -14,28 +13,33 @@ import javafx.scene.layout.StackPane;
  * Visual component for Messages
  */
 public class MessageShape extends Region {
+    private MessageData messageData;
+    private Message message;
+
     private StackPane containerPane;
     private Label contentLabel;
     private Button closeButton;
 
     public MessageShape(Message msg) { //TODO different colours for different types of message
+        messageData = MessageData.getInstance();
+        message = msg;
+
         containerPane = new StackPane();
         contentLabel = new Label(msg.getContent());
         contentLabel.setStyle("-fx-border-color:#FF9999BB; -fx-border-radius:5px; " +
                 "-fx-background-color:#FF999999; -fx-background-radius:5px;" +
-                "-fx-text-fill:#882222FF;" +
-                "-fx-padding:10px");
+                "-fx-text-fill:#771111FF; -fx-font-size:12px;" +
+                "-fx-padding:3 15 3 10");
         closeButton = new Button("x");
         containerPane.getChildren().addAll(contentLabel, closeButton);
         StackPane.setAlignment(closeButton, Pos.TOP_RIGHT);
-        StackPane.setMargin(contentLabel, new Insets(6, 6, 6, 6));
+        StackPane.setMargin(contentLabel, new Insets(3, 6, 3, 0));
 
         closeButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
-                // remove itself from the parent when closed
-                Pane parent = (Pane) getParent();
-                parent.getChildren().remove(containerPane.getParent());
+                // remove itself from the MessageData when closed
+                messageData.getMessageList().remove(message);
             }
         });
 
